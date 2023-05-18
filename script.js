a3d73546c6ac6e38127fac3febef53cb8e247d8f
abhay14gohel <114091511+abhay14gohel@users.noreply.github.com>
2023-05-18 18:54:54 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+var element = document.getElementById("myElement");
+var width = element.offsetWidth; // Get the current width of the element
+var height = width ; // Calculate the desired height as 75% of the width
+element.style.height = height + "px"; // Apply the height to the element
